68ab906b0e2890c57eaecc9a5e2cc28ef204bf41
李力 <zzuieliyaoli@gmail.com>
2017-06-23 12:21:47 +0800

    Add dev

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,9 @@ var postcss = require('gulp-postcss');
 var cssmin = require('gulp-cssmin');
 var salad = require('postcss-salad')(require('./salad.config.json'));
 var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
 
-gulp.task('compile', function() {
+gulp.task('compile:build', function() {
   return gulp.src('./src/*.css')
     .pipe(postcss([salad]))
     .pipe(cssmin())
@@ -19,4 +20,31 @@ gulp.task('copyfont', function() {
     .pipe(gulp.dest('./lib/fonts'));
 });
 
-gulp.task('build', ['compile', 'copyfont']);
+gulp.task('build', ['compile:build', 'copyfont']);
+
+gulp.task('compile:dev', function () {
+  return gulp.src('./src/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(postcss([salad]))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dev'));
+});
+
+gulp.task('compile:bs', ['compile:dev'], function () {
+  browserSync.reload();
+});
+
+gulp.task('dev', function() {
+  browserSync.init({
+    server: './',
+    port: 9849,
+    open: false,
+    watchOptions: {
+      ignoreInitial: true,
+      ignored: '*.map'
+    },
+  });
+
+  gulp.watch('./src/*.css', ['compile:bs']);
+  gulp.watch('*.html').on('change', browserSync.reload);
+});
